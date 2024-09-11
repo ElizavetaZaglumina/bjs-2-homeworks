6b26bbd66dea954a42ea73ec5ca77be54ed97a8f
Elizaveta Zaglumina <zaglumina.elizaveta@gmail.com>
2024-09-11 12:48:49 +0300

    3.arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,15 @@
 function compareArrays(arr1, arr2) {
+    return (
+      arr1.length === arr2.length &&
+      arr1.every((item, index) => item === arr2[index])
+    );
+  }
   
-}
-
-function getUsersNamesInAgeRange(users, gender) {
-  
-}
+  function getUsersNamesInAgeRange(users, gender) {
+    const filteredUsers = users.filter(user => user.gender === gender);
+    if (filteredUsers.length === 0) {
+      return 0;
+    }
+    const averageAge = filteredUsers.reduce((sum, user) => sum + user.age, 0);
+    return averageAge / filteredUsers.length;
+  }
